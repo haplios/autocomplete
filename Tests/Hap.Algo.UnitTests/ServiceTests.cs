575a7ea9cd8d56cf9931bb4732a3c57d649fac43
Neil Schurrer <nschurrer@toppers.com>
2024-09-25 06:42:40 -0500

    wip

--- Tests/Hap.Algo.UnitTests/ServiceTests.cs
+++ Tests/Hap.Algo.UnitTests/ServiceTests.cs
@@ -9,18 +9,30 @@ internal class ServiceTests
     private IWordListFileSource _wordListFileSource;
     private ITrieService _trieService;
     private const string FILE_NAME = "./words_alpha.txt";
+    private string[] _words;
 
     [SetUp]
-    public void Setup()
+    public async Task Setup()
     {
         _wordListFileSource = new WordListFileSource();
         _trieService = new TrieService();
+        _words = await _wordListFileSource.GetWords(FILE_NAME);
     }
 
     [Test]
     public void TrieService_BuildTrie()
     {
-        //var results = await _trieService.BuildTrie();
-        //Assert.That(results.Children.Count == 26);
+        var results = _trieService.BuildTrie(_words);
+        Assert.That(results.Root().Children.Count == 26);
+    }
+
+    [Test]
+    public void TrieService_SearchTrie()
+    {
+        var results = _trieService.BuildTrie(_words);
+
+        var result = results.FindNode("agent".ToCharArray(), null);
+
+        Assert.That(result?.IsEndOfWord == true);
     }
 }
